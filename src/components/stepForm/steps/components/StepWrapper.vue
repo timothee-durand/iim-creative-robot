bdd4f943c9a49fc927ffd91862f0d5490bba9f52
Timothée DURAND <53626355+timothee-durand@users.noreply.github.com>
2024-01-31 14:03:44 +0100

    feat: add identity step (#3)

--- src/components/stepForm/steps/components/StepWrapper.vue
+++ src/components/stepForm/steps/components/StepWrapper.vue
@@ -19,26 +19,31 @@
 </template>
 
 <style scoped lang="scss">
+@import "../vars";
 .wrapper {
   padding: 0 2rem;
+  width: 100%;
 }
 .step {
   display: flex;
   flex-direction: column;
   gap: 2rem;
   text-align: left;
-  max-width: 1060px;
+  max-width: $stepMaxWidth;
+  width: 100%;
+  margin: 0 auto;
+  position: relative;
 
   &__question {
     font-size: 2rem;
     font-weight: bold;
     margin-bottom: 1rem;
+    text-transform: uppercase;
   }
   &__input {
     display: flex;
     justify-content: space-between;
     gap: 1rem;
-    margin: 0 -25px ;
   }
 }
 </style>
