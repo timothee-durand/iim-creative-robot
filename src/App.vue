add9c39bc7fffcd0cd1bbc9f451120ac3f8760f0
Pierre-Alexis Maerten <pierre-alexis.maerten@edu.devinci.fr>
2024-01-31 15:44:38 +0100

    rajout du header

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import StepForm from "./components/stepForm/StepForm.vue";
+import Header from "./components/header/Header.vue";
 import {useFormStepStore} from "./components/stepForm/formStore.ts";
 
 const store = useFormStepStore()
@@ -9,6 +10,7 @@ function onSubmit() {
 </script>
 
 <template>
+  <Header />
   <StepForm  @submit="onSubmit"/>
 </template>
 
