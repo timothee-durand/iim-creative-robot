0b10a127632a179fbd37680ec5b785ee83ba3899
Timothée DURAND <53626355+timothee-durand@users.noreply.github.com>
2024-02-01 10:19:40 +0100

    feat: use processor in animation speed (#7)
    * feat: use animation speed
    
    * feat: add slow button
    
    * fix: after merge

--- src/components/stepForm/steps/identity/robot.vue
+++ src/components/stepForm/steps/identity/robot.vue
@@ -1,50 +1,61 @@
+<script setup lang="ts">
+import {computed} from "vue";
+import {useStore} from "../../../base/styleStore.ts";
+
+const cssStore = useStore()
+const styles = computed(() => ({
+      '--robot-color': cssStore.background,
+    })
+)
+</script>
+
 <template>
-<svg width="692" height="221" viewBox="0 0 692 221" fill="none" xmlns="http://www.w3.org/2000/svg">
-<g filter="url(#neon)">
-  <rect x="661" y="27" width="6" height="46" rx="3" fill="var(--robot-color)"/>
-  <path d="M617 25H661C664.314 25 667 27.6863 667 31V31H617V25Z" fill="var(--robot-color)"/>
-  <path d="M617 25H623V74V74C619.686 74 617 71.3137 617 68V25Z" fill="var(--robot-color)"/>
-  <path d="M617 69H667V69C667 72.3137 664.314 75 661 75H623C619.686 75 617 72.3137 617 69V69Z" fill="var(--robot-color)"/>
-</g>
-<g filter="url(#neon)">
-  <circle cx="634" cy="42" r="4" fill="var(--robot-color)"/>
-</g>
-<g filter="url(#neon)">
-  <circle cx="650" cy="42" r="4" fill="var(--robot-color)"/>
-</g>
-<line x1="0.5" y1="76" x2="0.500002" y2="26" stroke="white"/>
-<path d="M0 47.5H592H593C593.268 21.4968 614.433 1 640.5 1C666.734 1 688 22.2665 688 48.5C688 74.5667 667.503 95.2318 641.5 95.5L641.5 96.0091L640.5 151.009L2.05216 218.497" stroke="white"/>
-<path class="animated-path" d="M0 47.5H592H593C593.268 21.4968 614.433 1 640.5 1C666.734 1 688 22.2665 688 48.5C688 74.5667 667.503 95.2318 641.5 95.5L641.5 96.0091L640.5 151.009L2.05216 218.497" stroke="white" stroke-width="3"/>
-<defs>
-  <filter id="neon" x="592" y="0" width="100" height="100" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-    <feFlood flood-opacity="0" result="BackgroundImageFix"/>
-    <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
-    <feOffset/>
-    <feGaussianBlur stdDeviation="7.5"/>
-    <feComposite in2="hardAlpha" operator="out"/>
-    <feColorMatrix type="matrix" values="0 0 0 0 0.8 0 0 0 0 0.2 0 0 0 0 0.792157 0 0 0 1 0"/>
-    <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_93_47"/>
-    <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
-    <feOffset/>
-    <feGaussianBlur stdDeviation="7.5"/>
-    <feComposite in2="hardAlpha" operator="out"/>
-    <feColorMatrix type="matrix" values="0 0 0 0 0.8 0 0 0 0 0.2 0 0 0 0 0.792157 0 0 0 1 0"/>
-    <feBlend mode="normal" in2="effect1_dropShadow_93_47" result="effect2_dropShadow_93_47"/>
-    <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
-    <feOffset/>
-    <feGaussianBlur stdDeviation="12.5"/>
-    <feComposite in2="hardAlpha" operator="out"/>
-    <feColorMatrix type="matrix" values="0 0 0 0 0.8 0 0 0 0 0.2 0 0 0 0 0.792157 0 0 0 1 0"/>
-    <feBlend mode="normal" in2="effect2_dropShadow_93_47" result="effect3_dropShadow_93_47"/>
-    <feBlend mode="normal" in="SourceGraphic" in2="effect3_dropShadow_93_47" result="shape"/>
-  </filter>
-</defs>
-</svg>
+  <svg width="692" height="221" viewBox="0 0 692 221" fill="none" xmlns="http://www.w3.org/2000/svg" :style="styles">
+    <g filter="url(#sofGlow)">
+      <circle cx="634" cy="42" r="4" fill="var(--robot-color)"/>
+      <circle cx="650" cy="42" r="4" fill="var(--robot-color)"/>
+      <path
+          d="M666.11 27.87C665.06 26.15 663.16 25 661 25H617V68C617 68.34 617.04 68.67 617.09 69H617C617 72.31 619.69 75 623 75H661C663.17 75 665.06 73.85 666.11 72.13C666.66 71.59 667 70.83 667 70V30C667 29.17 666.66 28.42 666.11 27.87ZM661 69H623V31H661V69Z"
+          fill="var(--robot-color)"/>
+    </g>
+    <line x1="0.5" y1="76" x2="0.500002" y2="26" stroke="white"/>
+    <path
+        d="M0 47.5H592H593C593.268 21.4968 614.433 1 640.5 1C666.734 1 688 22.2665 688 48.5C688 74.5667 667.503 95.2318 641.5 95.5L641.5 96.0091L640.5 151.009L2.05216 218.497"
+        stroke="white"/>
+    <path class="animated-path"
+          d="M0 47.5H592H593C593.268 21.4968 614.433 1 640.5 1C666.734 1 688 22.2665 688 48.5C688 74.5667 667.503 95.2318 641.5 95.5L641.5 96.0091L640.5 151.009L2.05216 218.497"
+          stroke="white" stroke-width="3"/>
+    <defs>
+
+      <filter id="sofGlow" height="300%" width="300%" x="-75%" y="-75%">
+        <!-- Thicken out the original shape -->
+        <feMorphology operator="dilate" radius="4" in="SourceAlpha" result="thicken"/>
+
+        <!-- Use a gaussian blur to create the soft blurriness of the glow -->
+        <feGaussianBlur in="thicken" stdDeviation="10" result="blurred"/>
+
+        <!-- Change the colour -->
+        <feFlood flood-color="var(--robot-color)" result="glowColor"/>
+
+        <!-- Color in the glows -->
+        <feComposite in="glowColor" in2="blurred" operator="in" result="softGlow_colored"/>
+
+        <!--	Layer the effects together -->
+        <feMerge>
+          <feMergeNode in="softGlow_colored"/>
+          <feMergeNode in="SourceGraphic"/>
+        </feMerge>
+
+      </filter>
+
+    </defs>
+  </svg>
 </template>
 <style scoped>
 svg {
   --robot-color: #CC33CA;
 }
+
 .animated-path {
   animation: path 6s linear infinite;
   stroke-dasharray: 80px;
