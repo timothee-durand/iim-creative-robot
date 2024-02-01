463ce7589a2fcf9e50338d5896ae2e20457f82ae
Pierre-Alexis Maerten <pierre-alexis.maerten@edu.devinci.fr>
2024-02-01 10:12:20 +0100

    feat/hero-header

--- src/components/heroHeader/heroHeader.vue
+++ src/components/heroHeader/heroHeader.vue
@@ -1,11 +1,20 @@
+<script setup lang="ts">
+import {useStore} from "../../components/base/styleStore.ts";
+
+const store = useStore();
+</script>
+
 <template>
     <div class="hero-header">
         <div class="hero-header__wrapper">
             <div class="hero-header__web">
-                <div class="hero-header__web__clip"></div>
+                <div class="hero-header__web__clip">
+                    <img src="../../assets/robot-header.png" class="hero-header__img" alt="robot lady">
+                </div>
             </div>
-            <span class="hero-header__date"></span>
-            <h1 class="hero-header__title"></h1>
+            <span class="hero-header__date">/2050</span>
+            <h1 :class="`hero-header__title title-back ${store.font} ${ isBlinking ? 'blinking' : '' }`"
+          :style="{'text-shadow' : `0 0 25px ${store.background}, 0 0 25px ${store.background}, 0 0 25px ${store.background}`, '-webkit-text-stroke-color': store.background, opacity: store.animation === 'ecologic' ? 0 : 1}">Cyber <br><span class="hero-header__title-float">Spark</span></h1>
         </div>
     </div>
 </template>
@@ -13,18 +22,42 @@
 <style scoped lang="scss">
 .hero-header {
     &__wrapper {
+        position: relative;
         height: 650px;
+        margin: 0 110px 0 20vw;
     }
     &__web {
-        clip-path: polygon(0 0, 100% 0, 100% 100%, 65% 100%);
-        border: 1px solid white;
+        clip-path: polygon(0 0, 100% 0, 100% 100%, 55% 100%);
+        background: white;
         height: 100%;
+        width:100%;
+        position: relative;
         &__clip {
             position: absolute;
-            top: 2px; 
-            left: 2px;
-            clip-path: polygon(0 0, 100% 0, 100% 100%, 65% 100%);
+            top: 0px; 
+            left: 1px;
+            width: calc(100% - 2px);
+            height: calc(100% - 1px);
+            background: black;
+            clip-path: polygon(0 0, 100% 0, 100% 100%, 55% 100%);
         }
+    }
+    &__date {
+        position: absolute;
+        top: 70px;
+    }
+    &__title {
+        position: absolute;
+        top: 80px;
+        font-size: 120px;
+        &-float {
+            margin-left: 60px;
+        }
+    }
+    &__img {
+        position: absolute;
+        bottom: -10px;
+        left: 50%;
     }
 }
 </style>
