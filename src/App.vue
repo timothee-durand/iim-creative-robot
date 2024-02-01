2d947676cef24caccc7b7a99de8533b0b64d266b
Karen <70762399+Karen160@users.noreply.github.com>
2024-02-01 10:15:22 +0100

    Feature/anim button (#8)
    * :construction: Animation
    
    * :dizzy: Heart fall

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,8 @@ import StepForm from "./components/stepForm/StepForm.vue";
 import Header from "./components/header/Header.vue";
 import HeroHeader from "./components/heroHeader/heroHeader.vue";
 import {useFormStepStore} from "./components/stepForm/formStore.ts";
-import {useStore} from "./components/base/styleStore.ts";
 
 const store = useFormStepStore()
-const storeCss = useStore()
 function onSubmit() {
   console.log('submit', store.$state)
 }
